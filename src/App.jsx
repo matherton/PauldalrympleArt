8cd46bd86eff9281a108164c0b09ed0900b2f992
Mark Atherton <mark@insightinternet.co.uk>
2024-07-04 19:10:34 +0100

    got npm react-image-gallery working with default images

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import FeaturedImageGallery from "./components/FeaturedImageGallery";
 import MixGallery from "./components/MixGallery";
-import ImageCarousel from "./components/ImageCarousel";
+import MyGallery from "./components/ImageCarousel";
 import Footer from "./components/Footer";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Header />
       <FeaturedImageGallery />
       <MixGallery />
-      {/* <ImageCarousel />*/}
+      <MyGallery />
       <Footer />
     </>
   );
